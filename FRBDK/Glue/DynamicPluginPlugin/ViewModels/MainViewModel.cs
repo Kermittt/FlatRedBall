1cedaa33cc965854acf3d9ab4c962a74c7697cdf
Nathan Keir <kermitt@bigpond.net.au>
2023-05-06 15:58:40 +1000

    wip: Adding tab UI.

--- FRBDK/Glue/DynamicPluginPlugin/ViewModels/MainViewModel.cs
+++ FRBDK/Glue/DynamicPluginPlugin/ViewModels/MainViewModel.cs
@@ -1,8 +1,40 @@
 ﻿using FlatRedBall.Glue.MVVM;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace DynamicPluginPlugin.ViewModels
 {
     public class MainViewModel : ViewModel
     {
+        public MainViewModel()
+        {
+            AddCommand = new CommandBase(AddExecute);
+            RemoveCommand = new CommandBase(RemoveExecute, RemoveCanExecute);
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand RemoveCommand { get; }
+        public ObservableCollection<PluginViewModel> Plugins { get; } = new();
+
+        public PluginViewModel SelectedPlugin
+        {
+            get => Get<PluginViewModel>();
+            set => Set(value);
+        }
+
+        private void AddExecute()
+        {
+
+        }
+
+        private void RemoveExecute()
+        {
+
+        }
+
+        private bool RemoveCanExecute()
+        {
+            return SelectedPlugin != null;
+        }
     }
 }
---
+++ FRBDK/Glue/DynamicPluginPlugin/ViewModels/PluginViewModel.cs
@@ -0,0 +1,31 @@
+﻿using FlatRedBall.Glue.MVVM;
+
+namespace DynamicPluginPlugin.ViewModels
+{
+    public class PluginViewModel : ViewModel
+    {
+        public string Name
+        {
+            get => Get<string>();
+            private set => Set(value);
+        }
+
+        public string FullPath
+        {
+            get => Get<string>();
+            private set => Set(value);
+        }
+
+        public bool IsEnabled
+        {
+            get => Get<bool>();
+            private set => Set(value);
+        }
+
+        public string Version
+        {
+            get => Get<string>();
+            private set => Set(value);
+        }
+    }
+}
